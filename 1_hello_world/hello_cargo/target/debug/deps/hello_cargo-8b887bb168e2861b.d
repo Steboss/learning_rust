7ddabb0b43f6fd65312f78184f7d1d50fdf23c0b
Steboss <stefanobosisio1@gmail.com>
2018-04-25 21:06:13 +0100

    Learning helloworld and cargo

---
+++ 1_hello_world/hello_cargo/target/debug/deps/hello_cargo-8b887bb168e2861b.d
@@ -0,0 +1,5 @@
+/home/steboss/projects/learning_rust/1_hello_world/hello_cargo/target/debug/deps/hello_cargo-8b887bb168e2861b: src/main.rs
+
+/home/steboss/projects/learning_rust/1_hello_world/hello_cargo/target/debug/deps/hello_cargo-8b887bb168e2861b.d: src/main.rs
+
+src/main.rs:
---
+++ 1_hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/steboss/projects/learning_rust/1_hello_world/hello_cargo/target/debug/hello_cargo: /home/steboss/projects/learning_rust/1_hello_world/hello_cargo/src/main.rs
